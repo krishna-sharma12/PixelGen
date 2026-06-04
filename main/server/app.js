8b1f9e0e28f67d9a4f771c768fb24942a15dd428
krishna-sharma12 <153375005+krishna-sharma12@users.noreply.github.com>
2026-06-04 17:30:39 +0000

    write main file

---
+++ main/server/app.js
@@ -0,0 +1,13 @@
+const express=require("express");
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send(" server is running fine ");
+})
+
+
+const port=5000;
+
+app.listen(port,() => {
+    console.log(`server is running  at ${port}`);
+})
