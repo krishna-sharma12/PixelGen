cff865ec1534a627353b5b161dc8d916fb870eaf
krishna-sharma12 <153375005+krishna-sharma12@users.noreply.github.com>
2026-06-04 20:28:49 +0000

    mongodb setup

--- main/server/app.js
+++ main/server/app.js
@@ -1,5 +1,13 @@
 const express=require("express");
 const app=express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+
 
 app.get("/",(req,res)=>{
     res.send(" server is running fine ");
