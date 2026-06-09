58efa82fd8a7301afdb31976eea810043771db71
krishna-sharma12 <153375005+krishna-sharma12@users.noreply.github.com>
2026-06-09 19:51:45 +0000

    backend works fine

--- main/server/app.js
+++ main/server/app.js
@@ -2,6 +2,13 @@ const express=require("express");
 const app=express();
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const generateImage = require("./services/imageservice");
+
+
+app.use(express.json());
+const cors = require("cors");
+
+app.use(cors());
 
 dotenv.config();
 
@@ -12,6 +19,44 @@ connectDB();
 app.get("/",(req,res)=>{
     res.send(" server is running fine ");
 })
+
+// app.get("/test", (req, res) => {
+//   res.json({
+//     success: true,
+//     message: "Backend working"
+//   });
+// });
+
+
+// app.post("/api/image/generate", (req, res) => {
+//   console.log("REQUEST RECEIVED");
+//   console.log(req.body);
+
+//   res.json({
+//     success: true
+//   });
+// });
+
+// api image prompt
+app.post("/api/image/generate", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const image = await generateImage(prompt);
+
+    res.json({
+      success: true,
+      image,
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    res.status(500).json({
+      success: false,
+      message: "Image generation failed",
+    });
+  }
+});
 
 
 const port=5000;
