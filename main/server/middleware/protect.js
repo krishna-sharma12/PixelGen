16c8bcca6ab18ce664dbaa79ea3eca1a3a51a16d
krishna-sharma12 <153375005+krishna-sharma12@users.noreply.github.com>
2026-06-24 13:36:21 +0000

    added protect route

--- main/server/middleware/protect.js
+++ main/server/middleware/protect.js
@@ -1,14 +1,27 @@
 const jwt=require("jsonwebtoken");
 const User = require("../models/User");
-const protect=async(req,res,next)=>{
-let token = req.headers.authorization;
-if(!token)
-  {
-    throw new Error("No token provided");
+
+  const protect=async(req,res,next)=>{
+  try{
+    let token = req.headers.authorization;
+    if(!token){
+     throw new Error("No token provided");
+    }
+    token=token.split(" ")[1];
+    const decode=jwt.verify(token,process.env.JWT_SECRET);
+    const user=await User.findById(decode.userId)
+    if(!user){
+     throw new Error("User not found");
+    }
+    req.user=user;
+    next();
   }
-  const decode=jwt.verify(token,process.env.JWT_SECRET);
-  const user=await User.findById(decode.userId)
-  req.user=user;
-  next();
+  catch(err){
+  throw new Error("Not authorized");
 }
+}
+
+
+
+
 module.exports={protect};
--- main/server/routes/authRoutes.js
+++ main/server/routes/authRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router=express.Router();
 const {validateRegister} = require("../middleware/validateRegister.js");
 const {validateLogin} = require("../middleware/validateLogin.js");
-// const {validateLogin} = require("../middleware/validateLogin");
+const{protect}=require("../middleware/protect.js");
+
 const {registerUser} = require("../controllers/authController");
 const{loginUser}=require("../controllers/authController")
 
@@ -15,6 +16,13 @@ router.post(
     "/login",
     validateLogin,
    loginUser
+)
+router.get(
+   "/protected",
+   protect,
+   (req,res)=>{
+      res.json(req.user);
+   }
 )
 // router.post(
 //     "/logout",
